25d5650cb786e58bd228a93e5a62268b490ce269
Ewald Van Rooyen <ewald.vanrooyen@peachpayments.com>
2021-04-08 09:07:05 +0200

    Server graceful shutdown functionality

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,12 @@ import app from "./app";
 const port = process.env.PORT || 4000;
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Server is listening on port ${port}!`));
 
+const shutdown = (done: () => {}) => {
+    server.close(done);
+};
+
+process.on("exit", shutdown.bind(null, process.exit));
+process.on("SIGINT", shutdown.bind(null, process.exit));
+process.on("uncaughtException", shutdown.bind(null, process.exit));
